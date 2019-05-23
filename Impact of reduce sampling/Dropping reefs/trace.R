5162ff8b85f089e3b75c788aa9170b748e7c3323
Pubudu Thilan <pubudupbd@gmail.com>
2019-05-23 23:16:38 +1000

    Calculate trace for KLD calculation

---
+++ Impact of reduce sampling/Dropping reefs/trace.R
@@ -0,0 +1,9 @@
+############################################################################
+## Project: Bayesian optimal design: improving the effectiveness of reef monitoring with time varying covariate information
+## Script purpose: Calculate trace for KLD calculation
+## Author: Pubudu Thialn
+
+trace <- function(M){
+  tr <- sum(diag(M))
+  tr
+}
